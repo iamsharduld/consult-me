8936f992acc177dde6861f1ab2b6a16c995d9b54
iamsharduld <iamsharduld@gmail.com>
2020-07-11 15:23:41 +0530

    firebase config for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAEFrBPDzZmemYKErn23YAUM8mEsjdAgE8",
+    authDomain: "consult-me-1447f.firebaseapp.com",
+    databaseURL: "https://consult-me-1447f.firebaseio.com",
+    projectId: "consult-me-1447f",
+    storageBucket: "consult-me-1447f.appspot.com",
+    messagingSenderId: "306937574528",
+    appId: "1:306937574528:web:1741716a33d213a752e265",
+    measurementId: "G-58PLHRW6NK"
+  }
 };
