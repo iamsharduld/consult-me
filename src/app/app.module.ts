29eee7bf4a018e8ea7b49952023167f8cefdfe59
iamsharduld <iamsharduld@gmail.com>
2020-07-12 18:54:37 +0530

    added Toolbar with icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { environment } from '../environments/environment';
 import { HomeComponent } from './home/home.component';
@@ -41,7 +42,8 @@ import { HomeComponent } from './home/home.component';
     MatIconModule,
     MatButtonModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatToolbarModule
   ],
   providers: [AuthGuard],
   bootstrap: [AppComponent]
